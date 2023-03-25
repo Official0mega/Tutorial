3f9900286fa8fff633a5f0b7abae776bf56e664a
Official0mega <supr3m30m3ga@gmail.com>
2023-03-25 23:07:21 +0100

    getrichquick.sh

---
+++ getrichquick.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "What is your Name?"
+
+read Name
+
+echo "How old are you?"
+
+read Age
+
+echo "Hello $Name, you're $Age years old"
+
+
+echo "$SHELL, $RANDOM, $PWD, $USER, $HOSTNAME"
