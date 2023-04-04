b551b1e0c3c2d212dc6a2fca0ee0a9dd9afcc225
Official0mega <supr3m30m3ga@gmail.com>
2023-04-04 12:25:18 +0100

    if and else statement sample

---
+++ if_else.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - if and else statement 1.
+ *
+ * Return: NULL
+ */
+
+int main(void)
+{
+		int age = 18;
+
+		if (age >= 25)
+		{
+			printf("You are Eligible to Proceed");
+		}
+
+		else
+		{
+			printf("Sorry, you're not allowed");
+		}
+		
+		return(0);
+}
